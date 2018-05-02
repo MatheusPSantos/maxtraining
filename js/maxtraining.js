27b9a520c55330c7cfe09a15720c89a0c54843fd
matheuspsantos <matheuspereira8187@gmail.com>
2018-05-02 03:54:32 -0300

    jquery inserido

---
+++ js/maxtraining.js
@@ -0,0 +1,13 @@
+/*
+    Maxtraining
+    Arquivo Javascript do site
+
+    Aqui estão os códigos em javascript para a página institucional do site
+
+    @author: Matheuspsantos
+    @links: https://github.com/matheuspsantos
+            https://facebook.com/matheus064
+            matheuspereira8187@gmail.com
+    2018-2019
+*/
+
